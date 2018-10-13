d706bf667704285f815a3d70b437c3153e42ae8d
qPCR4vir <arielvina@yahoo.es>
2018-10-13 04:18:11 +0200

    linux vs windows/win32 fix

--- include/sdsl/memory_management.hpp
+++ include/sdsl/memory_management.hpp
@@ -291,7 +291,7 @@ inline void block_markfree(mm_block_t* ptr) { block_update(ptr, SETFREE(ptr->siz
 /* mark the block as used */
 inline void block_markused(mm_block_t* ptr) { block_update(ptr, UNMASK_SIZE(ptr->size)); }
 
-#ifndef MSVC_COMPILER
+#ifndef _WIN32   //  MSVC_COMPILER
 
 class hugepage_allocator {
 private:
@@ -713,7 +713,7 @@ private:
 public:
 	static uint64_t* alloc_mem(size_t size_in_bytes)
 	{
-#ifndef MSVC_COMPILER
+#ifndef _WIN32   // MSVC_COMPILER
 		auto& m = the_manager();
 		if (m.hugepages) {
 			return (uint64_t*)hugepage_allocator::the_allocator().mm_alloc(size_in_bytes);
@@ -723,7 +723,7 @@ public:
 	}
 	static void free_mem(uint64_t* ptr)
 	{
-#ifndef MSVC_COMPILER
+#ifndef _WIN32   // MSVC_COMPILER
 		auto& m = the_manager();
 		if (m.hugepages and hugepage_allocator::the_allocator().in_address_space(ptr)) {
 			hugepage_allocator::the_allocator().mm_free(ptr);
@@ -734,7 +734,7 @@ public:
 	}
 	static uint64_t* realloc_mem(uint64_t* ptr, size_t size)
 	{
-#ifndef MSVC_COMPILER
+#ifndef _WIN32   //  MSVC_COMPILERMSVC_COMPILER
 		auto& m = the_manager();
 		if (m.hugepages and hugepage_allocator::the_allocator().in_address_space(ptr)) {
 			return (uint64_t*)hugepage_allocator::the_allocator().mm_realloc(ptr, size);
@@ -746,12 +746,12 @@ public:
 public:
 	static void use_hugepages(size_t bytes = 0)
 	{
-#ifndef MSVC_COMPILER
+#ifndef _WIN32   // MSVC_COMPILER
 		auto& m = the_manager();
 		hugepage_allocator::the_allocator().init(bytes);
 		m.hugepages = true;
 #else
-		throw std::runtime_error("hugepages not support on MSVC_COMPILER");
+		throw std::runtime_error("hugepages not support on _WIN32    MSVC_COMPILER");
 #endif
 	}
 	template <class t_vec>
